ced436b7a5310c7d073e388c9467e333ff6a6278
ronnyjubhari <71800550+ronnyjubhari@users.noreply.github.com>
2022-05-22 16:11:09 +0700

    Add files via upload

---
+++ dweb201/latihan/latihan2/readme.md
@@ -0,0 +1,7 @@
+# Simple PWA example
+
+Online demo can be found on https://ronnyjubhari.github.io/pwa
+
+## Running locally
+
+Serve folder with your server of choice. For instance `npm install -g serve`.
---
+++ dweb201/latihan/latihan2/sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'cache-v1';
+
+const staticAssets = [
+  './',
+  './app.js'
+];
+
+self.addEventListener('install', async function () {
+  const cache = await caches.open(cacheName);
+  cache.addAll(staticAssets);
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
